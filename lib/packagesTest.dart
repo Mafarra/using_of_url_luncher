e808d667078f5e6a849228bfb1fa8c80dc62497a
Mfarra <mam.farra@gmail.com>
2021-01-11 00:19:25 +0300

    share post and photo imagePicker

--- lib/packagesTest.dart
+++ lib/packagesTest.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:share/share.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class UrlLuncher extends StatelessWidget {
+  sharePhoto() async {
+    final picker = ImagePicker();
+    final PickedFile pickedFile =
+        await picker.getImage(source: ImageSource.camera);
+    Share.shareFiles(['${pickedFile.path}'],
+        text: 'Nice Photo', subject: 'test image Picker');
+  }
+
+  sharePost(String post) {
+    Share.share(post);
+  }
+
   makeCall(int phoneNumer) async {
     if (await canLaunch('tel:$phoneNumer')) {
       await launch('tel:$phoneNumer');
@@ -74,6 +88,16 @@ class UrlLuncher extends StatelessWidget {
                   onPressed: () {
                     sendEmail('mam.farra2030@gmail.com', 'test@gmail.com',
                         'textEmail', 'test email for you');
+                  }),
+              RaisedButton(
+                  child: Text('Share Post'),
+                  onPressed: () {
+                    sharePost('this is my shared post');
+                  }),
+              RaisedButton(
+                  child: Text('Share Photo'),
+                  onPressed: () {
+                    sharePhoto();
                   }),
             ],
           ),
